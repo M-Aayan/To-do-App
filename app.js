1131ebbe4269b877f7ede8fe200c90c0573721db
Muhammad Aayan Shaikh <muhammadaayanshaikhforbusiness@gmail.com>
2026-07-09 02:02:48 +0500

    done and update feature added also dark and light mode added

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const TextBox = document.querySelector("#textbox")
 const addBtn = document.querySelector("#addBtn")
 const ulTaskList = document.querySelector("#tasklist")
+let currentTask = null;
 
+
+//dark/light mode
+
+const themeBtn = document.querySelector("#themeBtn");
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark")){
+        themeBtn.textContent = "☀️";
+    }else{
+        themeBtn.textContent = "🌙";
+    }
+});
 
 
 
@@ -19,20 +32,61 @@ const addTask = () => {
         TextBox.value = ""
         return
     }
+     if (currentTask !== null) {
+        currentTask.textContent = TextBoxValue;
+
+        currentTask = null;
+        TextBox.value = "";
+        addBtn.textContent = "Add";
+
+        return
+     }
+
         let li = document.createElement("li")
-        li.textContent = TextBoxValue
-        ulTaskList.appendChild(li)
-        TextBox.value = ""
+        let taskText = document.createElement("span");
+        taskText.textContent = TextBoxValue
+        li.appendChild(taskText)
 
         //Delete task
 
         const DelBtn = document.createElement("button")
         DelBtn.textContent = " Delete"
-        DelBtn.classList.add
+        DelBtn.classList.add("delete-btn")
         li.appendChild(DelBtn)
         DelBtn.addEventListener("click" , ()=>{
             li.remove()
         })
+
+        // Done Task 
+
+        const DoneBtn = document.createElement("button")
+        DoneBtn.textContent = "Done!"
+        DoneBtn.classList.add("done-btn")
+        li.appendChild(DoneBtn)
+        DoneBtn.addEventListener("click", ()=>{
+            li.classList.toggle("completed")
+        })
+
+        // Edit Task
+
+        const EditBtn = document.createElement("button");
+        EditBtn.textContent = "Edit";
+        EditBtn.classList.add("edit-btn")
+        li.appendChild(EditBtn);
+
+        EditBtn.addEventListener("click", () => {
+        TextBox.value = taskText.textContent;
+        currentTask = taskText;
+        addBtn.textContent = "Update";
+        TextBox.focus();
+
+});
+        
+    ulTaskList.appendChild(li);
+    TextBox.value = ""
+
+addBtn.addEventListener("click", addTask);
+
 }
 
 
